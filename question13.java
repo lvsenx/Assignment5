f7aed1760e7d0f9a43fd702246a887df8792d481
Levent Sen <35978091+lvsenx@users.noreply.github.com>
2020-10-08 16:44:50 +0200

    Update question13.java

--- question13.java
+++ question13.java
@@ -8,13 +8,13 @@ public class question13 {
 		
 		for(int i=1; i<=4;i++) {
 			for(int j=1;j<=4;j++) {
-				System.out.print("W");
-				System.out.print("B");
+				System.out.print("W ");
+				System.out.print("B ");
 			}
 			System.out.println();
 			for(int j=1;j<=4;j++) {
-				System.out.print("B");
-				System.out.print("W");
+				System.out.print("B ");
+				System.out.print("W ");
 			}
 			System.out.println();
 		}
